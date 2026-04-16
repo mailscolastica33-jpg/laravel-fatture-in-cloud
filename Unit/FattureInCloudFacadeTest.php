613a0e6c401d6b679b8acacea7cef45f56fd519e
cogs <mailscolastica33@gmail.com>
2026-04-16 10:54:47 +0200

    Added Account Test and Presets for other tests

---
+++ Unit/FattureInCloudFacadeTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Unit;
+
+use Mockery;
+use OfflineAgency\FattureInCloud\FattureInCloud;
+use OfflineAgency\FattureInCloud\FattureInCloudFacade;
+use OfflineAgency\FattureInCloud\FattureInCloudServiceProvider;
+use Orchestra\Testbench\TestCase;
+
+class FattureInCloudFacadeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_loads_facade_alias()
+    {
+        $this->app->singleton(
+            'fatture-in-cloud',
+            function ($app) {
+                return Mockery::mock(FattureInCloud::class, function ($mock) {
+                    $mock->shouldReceive('test');
+                });
+            });
+
+        \FattureInCloud::test();
+        $this->assertTrue(true);
+    }
+
+    public function getPackageProviders($app)
+    {
+        return [
+            FattureInCloudServiceProvider::class,
+        ];
+    }
+
+    public function getPackageAliases($app)
+    {
+        return [
+            'FattureInCloud' => FattureInCloudFacade::class,
+        ];
+    }
+}
